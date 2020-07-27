af28f0c8efbbbfaeb0116e2c11570f8af1fb5725
Víctor <victor.sanchez.sanchez@gmail.com>
2020-07-27 20:48:37 +0200

    fichero saludos

---
+++ Proyecto World/saludos.py
@@ -0,0 +1,17 @@
+print "Saludo en aleman"
+import aleman
+
+print "Saludo en castellano"
+import castellano
+
+print "Saludo en frances"
+import frances
+
+print "Saludo en ingles"
+import ingles
+
+print "Saludo en italiano"
+import italiano
+
+print "Saludo en finlandes"
+import finlandes
