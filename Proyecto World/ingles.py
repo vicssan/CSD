818c6e36bab3a4715af58164c9c576841b249e04
Víctor <victor.sanchez.sanchez@gmail.com>
2020-07-27 21:17:52 +0200

    Añadiendo saludos en inglés

--- Proyecto World/ingles.py
+++ Proyecto World/ingles.py
@@ -1,4 +1,7 @@
 def saludar():
-	print "Hello, folks!"
-	return
-saludar() 
+	print "Hello folks!"
+	print "What's up?"
+	print "The 4 Non Blondes song!"
+	print "Where have you been hiding?"
+return
+saludar()
